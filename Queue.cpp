489fa26ade148ba8109c3d31af0f8d5c42c1d81b
U-Spencer-MSI\Spencer <spencershadley@utexas.edu>
2015-12-01 14:58:32 -0600

    created generic queue

--- Queue.cpp
+++ Queue.cpp
@@ -1,54 +1,50 @@
+#include <stdio.h>
 #include <iostream>
+#include <utility>
+#include <deque>
 #include <vector>
 
-template<typename T, typename C = std::vector<T>>
+using namespace std::rel_ops;
+
+template <typename T, typename C = std::vector<T>>
 class my_queue {
 
 private:
-	C container;
-	int front = -1
-	int end = -1;
+	C collection;
 
 public:
-	explicit my_queue(const C& c = C()) : _container(c) {}
 
-	my_queue(const my_queue&) = default;
+	friend bool operator==(const my_queue& lhs, const my_queue& rhs) {
+		return lhs.collection == rhs.collection;
+	}
+
+	friend bool operator<(const my_queue& lhs, const my_queue& rhs) {
+		std::lexicographical_compare(lhs.collection.begin(), lhs.collection.end(), rhs.collection.end());
+	}
+
+	explicit my_queue(C c = C()) : collection(c) {}
+	my_queue(const my_queue& q) = default;
 	~my_queue() = default;
-	my_queue& operator= (const my_queue&) = default;
+	my_queue& operator=(const my_queue& rhs) = default;
 
 	bool empty() const {
-		return _container.empty();
+		return collection.empty();
 	}
 
 	void pop_front() {
-		if (front == -1)
-			throw std::invalid_argument("empty queue");
-		else {
-			if (front == end) front = end = -1;
-			front = (front == size() - 1) ? 0 : front+1;
-		}
-		_container.erase(_container.begin() + index % size());
+		collection.erase(collection.begin());
 	}
 
 	void push_back(const T& v) {
-		
-		if ((front == 0 && end == size() - 1) || (end + 1 == front))
-			throw std::invalid_argument("full queue");
-		else
-			end = (end == size() - 1) ? 0 : end+1;
-
-		_container[end] = v;
-
-		if (front == -1)
-			front = 0;
+		collection.push_back(v);
 	}
 
 	size_t size() const {
-		return _container.size();
+		return collection.size();
 	}
 
 	T& peek() {
-		return _container.front();
+		return collection.front();
 	}
 
 	const T& peek() const {
