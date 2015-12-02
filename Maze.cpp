d510a98cc102f317038dbef85e644b276f0362e1
U-Spencer-MSI\Spencer <spencershadley@utexas.edu>
2015-12-01 22:30:23 -0600

    fixed template thing for maze

--- Maze.cpp
+++ Maze.cpp
@@ -4,43 +4,44 @@
 
 /**
 
-======= PROBLEM =======
-you have these like two base classes
-ghost room and ghost maze or something
-and you have to extend the class to do something else
-but have a templatized version of maze that can accept ghost rooms and other kinds of rooms and doors and shit
-it's basically seeing if you learned about templates and virtual inheritence from life
+	======= PROBLEM =======
+	you have these like two base classes
+	ghost room and ghost maze or something
+	and you have to extend the class to do something else
+	but have a templatized version of maze that can accept ghost rooms and other kinds of rooms and doors and shit
+	it's basically seeing if you learned about templates and virtual inheritence from life
 
-sorry that was probably confusing
-you have a maze class, a room class and a door class
-a maze is composed of rooms and doors
-there are different kinds of rooms and doors that correspond to different mazes
-i think his example was ghost doors/rooms and some other kind of rooms and doors
-and he hard coded the maze types
-GhostMaze and OtherMaze
-your goal is to make a templatized maze class that can accept both ghost rooms/doors and the other rooms/doors
+	sorry that was probably confusing
+	you have a maze class, a room class and a door class
+	a maze is composed of rooms and doors
+	there are different kinds of rooms and doors that correspond to different mazes
+	i think his example was ghost doors/rooms and some other kind of rooms and doors
+	and he hard coded the maze types
+	GhostMaze and OtherMaze
+	your goal is to make a templatized maze class that can accept both ghost rooms/doors and the other rooms/doors
 
-There is no abstract Maze class
-there are two concrete types
-called GhostMaze and OtherMaze
-It's your job to make Maze<T>
-that can accept both ghostrooms/ghostdoors and otherrooms/otherdoors
-I think pretty much everyone made an abstract door and room type
-then you can use that in the methods and can recieve GhostDoor* and OtherDoor*
-using AbstractDoor*
-and it might not have been Maze<T> just Maze
-where the constructor takes an AbstractDoor* and an AbstractRoom*
-and depending on what you pass as you construct the maze objects determines what it is
-sorry it's been a while
+	There is no abstract Maze class
+	there are two concrete types
+	called GhostMaze and OtherMaze
+	It's your job to make Maze<T>
+	that can accept both ghostrooms/ghostdoors and otherrooms/otherdoors
+	I think pretty much everyone made an abstract door and room type
+	then you can use that in the methods and can recieve GhostDoor* and OtherDoor*
+	using AbstractDoor*
+	and it might not have been Maze<T> just Maze
+	where the constructor takes an AbstractDoor* and an AbstractRoom*
+	and depending on what you pass as you construct the maze objects determines what it is
+	sorry it's been a while
+
+	you cant change the code of GhostRoom and stuff to inhert from abstract room
 
 **/
 
-template<typename R, typename D>
 class Maze {
 
-	friend Maze& operator=(const Maze& lhs, const Maze& rhs) {
-		lhs.room = rhs.room; // will this just use pointer equality? Do I need to dereference like this -> [    *(lhs.room) = *(rhs.room);     ] ?
-		lhs.door = rhs.door;
+	Maze& operator=(const Maze& rhs) {
+		room = rhs.room; // will this just use pointer equality? Do I need to dereference like this -> [    *(lhs.room) = *(rhs.room);     ] ?
+		door = rhs.door;
 	}
 
 	AbstractRoom* room;
@@ -48,8 +49,13 @@ class Maze {
 
 public:
 	Maze(AbstractRoom* r, AbstractDoor* d) : room(r), door(d) {}
-	Maze(const Maze& m) : maze(&m) {}
-	~Maze() { delete maze; }
+	Maze(const Maze& m) : 
+		room(m.room), 
+		door(m.door) {}
+	~Maze() { 
+		delete room; 
+		delete door; 
+	}
 };
 
 class AbstractRoom {
@@ -57,6 +63,7 @@ protected:
 	size_t volume;
 	size_t height = 4;
 
+public:
 	virtual AbstractRoom* create()	const = 0; // essentially the default cstr
 	virtual AbstractRoom* clone()	const = 0; // essentially the copy cstr
 	virtual ~AbstractRoom() = 0;
@@ -68,9 +75,12 @@ protected:
 
 	// should there bea protected cstr, copy cstr?
 
-public:
-	void increase_volume(size_t amount) {
+	void increase_volume(size_t amount) { // now it's not an interface
 		volume += amount;
+	}
+
+	void increase_height(size_t amount) {
+		height += amount;
 	}
 
 	size_t get_area() {
